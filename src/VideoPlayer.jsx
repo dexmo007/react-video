edcf9451d7e2052d1b37c889845d82743f5f8613
Henrik Drefs <henrik.drefs@outlook.de>
2020-11-08 00:28:59 +0100

    control styling, pip and fullscreen

--- src/VideoPlayer.jsx
+++ src/VideoPlayer.jsx
@@ -1,5 +1,11 @@
 import { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components'
+import { ReactComponent as PlayIcon } from './icons/play.svg'
+import { ReactComponent as PauseIcon } from './icons/pause.svg'
+import { ReactComponent as Rwd10 } from './icons/rwd10.svg'
+import { ReactComponent as Fwd10 } from './icons/fwd10.svg'
+import { ReactComponent as FullscreenIcon } from './icons/fullscreen.svg'
+import { ReactComponent as PictureInPictureIcon } from './icons/pip.svg'
 
 const height = 300;
 
@@ -14,11 +20,56 @@ z-index: 0;
 
 const Controls = styled.div`
 position: absolute;
+top: 0;
+bottom: 0;
+left: 0;
+right: 0;
+opacity: ${props => props.hidden ? 0 : 1};
+`;
+
+const MainControls = styled.div`
+position: absolute;
 top: 50%;
 left: 50%;
 transform: translate(-50%, -50%);
 z-index: 1;
-opacity: ${props => props.hidden ? 0 : 1};
+
+display: inline-flex;
+
+& > :not(:last-child) {
+  margin-right: 25px;
+}
+& svg {
+  transform: scale(0.9);
+  transition: transform 100ms ease-in-out;
+  cursor: pointer;
+}
+& > :hover svg, & > svg:hover  {
+  transform: scale(1);
+}
+`;
+const RightBottomControls = styled.div`
+position: absolute;
+right: 0;
+bottom: 0;
+display: inline-flex;
+margin-bottom: 10px;
+
+& > * {
+  margin-right: 10px;
+}
+& > svg {
+   fill: #fff;
+   width: 20px;
+   height: 20px;
+   transform: scale(0.9);
+   transition: transform 100ms ease-in-out;
+   cursor: pointer;
+}
+& > svg:hover {
+  transform: scale(1);
+}
+
 `;
 
 function useDelayableState(initialState) {
@@ -46,12 +97,15 @@ function VideoPlayer({src}) {
   const [showControls, setShowControls, clearControlsTimeout] = useDelayableState(false)
 
   function togglePlaying() {
+    setShowControls(true)
+    setShowControls(false, 2000)
     const video = videoRef.current;
     if (video.paused) {
       video.play()
     } else {
       video.pause()
     }
+
   }
   function notifyState(state) {
     return () => setState(state)
@@ -64,6 +118,25 @@ function VideoPlayer({src}) {
       video.currentTime = Math.min(Math.max(0, value), video.duration)
     }
   }
+  async function enterPictureInPicture() {
+    if (videoRef.current !== document.pictureInPictureElement) {
+        await videoRef.current.requestPictureInPicture();
+    } else {
+      await document.exitPictureInPicture();
+    }
+  }
+  async function enterFullscreen(e) {
+    console.log(e);
+    const video = videoRef.current;
+    if (video.requestFullscreen)
+      video.requestFullscreen();
+    else if (video.webkitRequestFullscreen)
+      video.webkitRequestFullscreen();
+    else if (video.mozRequestFullscreen)
+      video.mozRequestFullscreen();
+    else if (video.msRequestFullscreen)
+      video.msRequestFullscreen();
+  }
   return (
     <Container    onMouseEnter={() => console.log('mouse enter') || setShowControls(true)}
     onMouseLeave={() => setShowControls(false, 2000)}
@@ -73,6 +146,7 @@ function VideoPlayer({src}) {
       }}
     >
       <video
+        id={Math.random()}
         ref={videoRef}
         src={src}
         onPlay={notifyState('playing')}
@@ -80,19 +154,24 @@ function VideoPlayer({src}) {
         onEnded={notifyState('ended')}
         height="100%"
         width="100%"
-
       ></video>
-      <Controls hidden={!showControls}
+      <Controls hidden={!showControls && false}
     >
-        <button onClick={() => setTime(-5, true)}>
+        <MainControls>
+        <Rwd10 onClick={() => setTime(-5, true)} fill="white" height="50px" width="50px">
           &lt;- 5s
-        </button>
-        <button onClick={togglePlaying}>
-          {state === 'playing' ? 'Pause' : 'Play'}
-        </button>
-        <button onClick={() => setTime(5, true)}>
+        </Rwd10>
+        <div onClick={togglePlaying}>
+          {state === 'playing' ? <PauseIcon fill="white" height="50px"/> : <PlayIcon fill="white" height="50px" width="50px"/>}
+        </div>
+        <Fwd10 onClick={() => setTime(5, true)} fill="white" height="50px" width="50px">
           5s -&gt;
-        </button>
+        </Fwd10>
+        </MainControls>
+        <RightBottomControls>
+          {document.pictureInPictureEnabled && <PictureInPictureIcon onClick={enterPictureInPicture}></PictureInPictureIcon>}
+          <FullscreenIcon onClick={enterFullscreen}></FullscreenIcon>
+        </RightBottomControls>
       </Controls>
       </Container>
 
